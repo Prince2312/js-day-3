9380bb9cdd0af4f59a12f1823c1c17b85eeb77db
Prince2312 <149226198+Prince2312@users.noreply.github.com>
2025-04-01 12:11:54 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not brouser
+
+// numbers => 2 to power 53
+// bigint
+//string =>""
+// boolean => true/false
+//null
+// undefined
+// symbol
+
+//object
