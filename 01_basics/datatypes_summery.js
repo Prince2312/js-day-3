845cf5af1e77b17f8dc3cee5432191a76e210abf
Prince2312 <149226198+Prince2312@users.noreply.github.com>
2025-04-02 10:58:00 +0000

    data type summery

---
+++ 01_basics/datatypes_summery.js
@@ -0,0 +1,18 @@
+// data types
+// two type
+// Primitive and non primitive
+
+// 7 type: string, Number, Boolean, null, undefined, Symbol,BigInt
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+const heros = ["shaktiman", "naagraj", "doga"];
+let myOBJ ={
+    name:"prince",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("Hello Prince");
+}
