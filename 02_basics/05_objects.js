b47690d2a42a2bc858db7281c93f60c4904d7612
Prince2312 <149226198+Prince2312@users.noreply.github.com>
2025-04-04 11:01:09 +0000

    objects  api

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,28 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "prince"
+}
+
+// course.courseInstructor
+
+const {courseInstructor} = course
+
+// console.logI(courseInstructor);
+console.log(instructor);
+
+const navbar = () => {
+
+}
+
+// const navbar =({company} =>{
+
+// })
+// navbar(company = "prince");
+
+// {
+
+//    "name":"prince",
+//    "coursename": "js in hindi",
+//    "price":"free"
+// }
