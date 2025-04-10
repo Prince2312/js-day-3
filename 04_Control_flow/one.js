7ca7de29027ebf2fc9b437c1e1ab226904219d8d
Prince2312 <149226198+Prince2312@users.noreply.github.com>
2025-04-10 14:12:50 +0000

    if , if else if , switch,, truthy and falsy

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,26 @@
+// if
+
+const isUserloggedIn = true;
+const temperature = 41;
+
+if(isUserloggedIn){
+    console.log("less trhan 50");
+} else {
+    console.log("temperature is greater than50");
+}
+
+console.log("exicute");
+
+const score = 200;
+
+if(score > 100){
+    let power = "fly";
+    console.log(`User power: ${power}`);
+}
+
+console.log(`User Power: ${power}`);
+
+const balance = 1000;
+if(balance > 500){
+    
+}
