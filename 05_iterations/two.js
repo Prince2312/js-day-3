613802f9c35424ba303338892e6be803c03462c2
Prince2312 <149226198+Prince2312@users.noreply.github.com>
2025-04-11 05:50:07 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// let index = 0;
+// while( index <= 10){
+//    console.log(`Value of index: ${index}`);
+//    index =  index+2;
+// } 
+
+// let Arr = ["batman","superman","flash","thor"];
+// let owl = 0;
+// while(owl <= Arr.length){
+//    console.log(`Value is: ${Arr[owl]}`);
+//    owl = owl +1
+// }
+
+// Do while loop
+
+let score = 1;
+do{
+    console.log(`score is: ${score}`)
+    score++
+} while(score <= 10);
