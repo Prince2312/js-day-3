e81cebda5082875f6a80ef6b7776d78054003971
Prince2312 <149226198+Prince2312@users.noreply.github.com>
2025-04-21 05:43:59 +0000

    second oops

--- 10_classes_and_oop/oops.js
+++ 10_classes_and_oop/oops.js
@@ -1,12 +1,36 @@
+// Object literals
+
 const user = {
     username:"prince",
     loginCount:8,
     signedIn:true,
 
-    getUserDetails: function(){
-         console.log("Got user details from database")
-    }
-}
+//    getUserDetails: function(){
+        // console.log("Got user details from database");
+ //       console.log(`username: ${this.username}`);
+ //       console.log(this);
+//    }
+ }
 
-console.log(user.getUserDetails);
-console.log(user.username);
+// console.log(user.getUserDetails);
+// console.log(user.username);
+// console.log(this);
+
+// const promiseOne = new Promise()
+// const date = new
+
+function User(username,loginCount,isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+    
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+
+    return this;
+}
+const userOne = new User("hitesh",12,true);
+const userTwo = new User("Prince singh",11,false);
+console.log(userOne.constructor);
+// console.log(userTwo);
